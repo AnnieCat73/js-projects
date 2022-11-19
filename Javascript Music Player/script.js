1c4d752019bc2fae93a6bcaa297b64420d277820
Annie  Indreiten <devannie777@gmail.com>
2022-11-19 05:18:51 +0000

    Song storing variables defined

--- Javascript Music Player/script.js
+++ Javascript Music Player/script.js
@@ -6,3 +6,8 @@ const songList = document.querySelector(".song-list");
 const title = document.querySelector("#title");
 const record = document.querySelector(".record");
 const volSlider = document.querySelector(".slider");
+
+let songArray = [];
+let songHeading = "";
+let songIndex = 0;
+let isPlaying = false;
