b86acf75bcfdd6aeb9d0ebd20363fd7030795843
Annie  Indreiten <devannie777@gmail.com>
2022-11-27 05:03:52 +0000

    Masterplay eventlistener

--- MasterPlay/script.js
+++ MasterPlay/script.js
@@ -1,1 +1,15 @@
 const music = new Audio("audio/Pray - Anno Domini Beats.mp3");
+
+let masterPlay = document.getElementById("masterPlay");
+
+masterPlay.addEventListener("click", () => {
+  if (music.paused || music.currentTime <= 0) {
+    music.play();
+    masterPlay.classList.remove("bi-play-fill");
+    masterPlay.classList.add("bi-pause-fill");
+  } else {
+    music.pause();
+    masterPlay.classList.add("bi-play-fill");
+    masterPlay.classList.remove("bi-pause-fill");
+  }
+});
