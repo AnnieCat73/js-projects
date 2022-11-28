246d8b3cf8db007251cd30f76dcd9f6d785c640a
Annie  Indreiten <devannie777@gmail.com>
2022-11-28 04:57:40 +0000

    MasterPlay addEventListener update

--- MasterPlay/script.js
+++ MasterPlay/script.js
@@ -13,3 +13,21 @@ masterPlay.addEventListener("click", () => {
     masterPlay.classList.remove("bi-pause-fill");
   }
 });
+/*
+let masterPlay = document.getElementById("masterPlay");
+//let wave = document.getElementsByClassName("wave")[0];
+/*from addEventListener down*
+console.log(masterPlay);
+masterPlay.addEventListener("click", () => {
+  if (music.paused || music.currentTime <= 0) {
+    music.play();
+    masterPlay.classList.remove("bi-play-fill");
+    masterPlay.classList.add("bi-pause-fill");
+    wave.classList.add("active2");
+  } else {
+    music.pause();
+    masterPlay.classList.add("bi-play-fill");
+    masterPlay.classList.remove("bi-pause-fill");
+    wave.classList.remove("active2");
+  }
+});*/
